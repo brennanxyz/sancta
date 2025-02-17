ac5c9fc50ceffe7b6f5035ff51a480b15306a179
brennanxyz <brennan@brennanharris.xyz>
2025-02-17 13:02:24 -0700

    added Person, Practice

--- THESES.md
+++ THESES.md
@@ -2,5 +2,6 @@
 Plural of the Latin _sanctum_, meaning a holy place.
 
 ## Theses
-1. Since Luther, a faith effort worth its salt should have an originating document.
-
+Since Luther, a faith effort worth its salt should have an originating document.
+1. Institutions of faith and transcendence are in decay and degeneracy.
+1. The goals and movements of capitalist reproduction conflict with human spiritual development.
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,8 @@
 //! The crate is inspired in part by the work of Terry Davis on TempleOS,
 //! creating a project in code specifically pursued as a work of religious
 //! devotion. The work in not carried out with a particular tradition as a
-//! guidepost, but instead seeks to formalize concepts that can be reliably
-//! applied to many (if not all) human faith and wisdom traditions. Perhaps
-//! it will be useful in the development of new systems of religious practice
-//! or in the synthesis or revival of new forms.
+//! guidepost. Perhaps it will be useful in the development of new systems of
+//! religious practice or in the synthesis or revival of old forms.
 
 #[allow(dead_code)] // haha
 /// Lack, emptiness, formlessness. Across traditions, the role and general
@@ -17,3 +15,20 @@
 /// chaotic evil, or the generative space that births all existence, void is a
 /// central and foundational concept to human transcental history.
 struct Void {}
+
+#[allow(dead_code)]
+/// A group (in whole or part) joined in common or collaborative practice
+struct Assembly {
+    people: Vec<Person>,
+    practices: Vec<Practice>,
+}
+
+#[allow(dead_code)]
+/// A human being
+struct Person {
+    name: String,
+    practices: Vec<Practice>,
+}
+
+#[allow(dead_code)]
+struct Practice {}
